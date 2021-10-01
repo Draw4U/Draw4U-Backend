bc2cc3c9059f770ff8e7d6d33204c7dec782af48
DOIL KIM <doili0552@gmail.com>
2021-10-01 09:46:08 +0900

    project setting

--- src/main/java/com/draw4u/Draw4uApplication.java
+++ src/main/java/com/draw4u/Draw4uApplication.java
@@ -1,7 +1,11 @@
 package com.draw4u;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Draw4uApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(Draw4uApplication.class, args);
     }
 }
